be3f5f12914978b66d14a7e30a637bfc6b5e4ee4
jsindt <j.sindt@ed.ac.uk>
2022-03-30 16:55:53 +0100

    Changing timetable

--- exercises/3-python/lammps_lj.py
+++ exercises/3-python/lammps_lj.py
@@ -1,16 +1,18 @@
 from mpi4py import MPI
 from lammps import lammps
 
-lmp = lammps()
-
-
-lmp.file("in.lj")
-
-for i in range(5) :
+def main():
+  lmp = lammps()
+  lmp.file("in.lj")
+  
+  for i in range(5) :
     lmp.command("log            log.%i" % i)
     
     cutoff = 1.1 + (0.1*i)
     lmp.command("pair_coeff      * * %f 1.0" % cutoff)
     lmp.command("run            100000")
+  
+  MPI.Finalize()
 
-MPI.Finalize()
+if __name__ == '__main__':
+    main()
