3b3330692e38c3fa90a4b0e78c5237eefe736781
Rui Apóstolo <r.apostolo@epcc.ed.ac.uk>
2024-01-10 16:50:11 +0000

    Exercises -- using new module

--- exercises/4-python/lammps_lj.py
+++ exercises/4-python/lammps_lj.py
@@ -1,18 +1,19 @@
-from mpi4py import MPI
+#from mpi4py import MPI
 from lammps import lammps
 
 def main():
-  lmp = lammps()
-  lmp.file("in.lj")
+    lmp = lammps()
+#    comm = MPI.COMM_WORLD
+    lmp.file("in.lj")
   
-  for i in range(5) :
-    lmp.command("log            log.%i" % i)
+    for i in range(5) :
+        lmp.command("log            log.1.%i" % i)
     
-    cutoff = 1.1 + (0.1*i)
-    lmp.command("pair_coeff      * * %f 1.0" % cutoff)
-    lmp.command("run            100000")
+        cutoff = 1.1 + (0.1*i)
+        lmp.command("pair_coeff      * * %f 1.0" % cutoff)
+        lmp.command("run            100000")
   
-  MPI.Finalize()
+#    MPI.Finalize()
 
 if __name__ == '__main__':
     main()
